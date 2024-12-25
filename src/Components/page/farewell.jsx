3e038ac8dfa83588e97be9e059d4e5c41196b418
Soulra01 <raden.raharja@binus.ac.id>
2024-12-25 19:54:59 +0700

    updt farewell

--- src/Components/page/farewell.jsx
+++ src/Components/page/farewell.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../Styles/Farewell.css";
-import { handleDateChange } from "./scripts/dateHandler"; // Import fungsi dari file terpisah
+import { handleDateChange } from "./scripts/dateHandler";
 import { processDate } from "./scripts/farewell";
 
 const Farewell = () => {
     const [date, setDate] = useState("");
     const navigate = useNavigate();
+
+    // Auto-refresh kontrol
+    useEffect(() => {
+        const hasRefreshed = sessionStorage.getItem("hasRefreshed") || "0";
+
+        if (parseInt(hasRefreshed, 10) < 2) {
+            sessionStorage.setItem("hasRefreshed", (parseInt(hasRefreshed, 10) + 1).toString());
+            window.location.reload();
+        }
+    }, []);
 
     const handleSubmit = () => {
         processDate(date, navigate);
@@ -15,14 +25,14 @@ const Farewell = () => {
     return (
         <div className="Background-img">
             <div className="date-input-container">
-                <label htmlFor="date-input">Tanggal Farewell-mu:    </label>
+                <label htmlFor="date-input">Tanggal Farewell-mu: </label>
                 <input
                     id="date-input"
                     type="text"
                     value={date}
-                    onChange={(e) => handleDateChange(e, setDate)} // Gunakan fungsi yang diimpor
+                    onChange={(e) => handleDateChange(e, setDate)}
                     placeholder="dd-mm-yy"
-                    maxLength="8" // Untuk membatasi panjang input menjadi 8 karakter (dd-mm-yy)
+                    maxLength="8"
                 />
                 <button onClick={handleSubmit}>Submit</button>
             </div>
