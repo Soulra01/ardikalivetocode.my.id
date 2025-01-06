5c98648e3738f7be74213d59f4db84a087cd7648
Soulra01 <raden.raharja@binus.ac.id>
2025-01-07 02:20:33 +0700

    web yaldi

--- src/Components/page/scripts/farewell.js
+++ src/Components/page/scripts/farewell.js
@@ -5,7 +5,10 @@ export const processDate = (date, navigate) => {
     // Statement if untuk pengalihan
     if (inputDate.getTime() === new Date(2024, 12, 13).getTime()) {
         navigate("/farewell/indah");
-    } else if (inputDate.getTime() === new Date(2024, 5, 5).getTime()) {
+    } else if (inputDate.getTime() === new Date(2024, 12, 31).getTime()) {
+        navigate("/farewell/Pak_RT");
+    }
+    else if (inputDate.getTime() === new Date(2024, 11, 31).getTime()) {
         navigate("/b");
     } else {
         alert("Yaaa tanggal farewell lu ga bener coba cek lagi dehh.... 😑");
